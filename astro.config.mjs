8fb778ca65d7fff41ee23c9b3688341356884483
lmajano07 <lmajano0701@gmail.com>
2023-09-12 12:52:22 -0600

    chore: remove additional config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,4 @@
 import { defineConfig } from "astro/config";
 
-import solidJs from "@astrojs/solid-js";
-
 // https://astro.build/config
-export default defineConfig({
-  site: import.meta.env.STRAPI_URL,
-  integrations: [solidJs()],
-});
+export default defineConfig({});
