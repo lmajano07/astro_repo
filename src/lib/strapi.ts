e7d6e05541358337109f67592c8f7070fe54fccc
lmajano07 <lmajano0701@gmail.com>
2023-09-12 18:58:07 -0600

    chore: added env var

--- src/lib/strapi.ts
+++ src/lib/strapi.ts
@@ -23,9 +23,7 @@ export default async function fetchApi<T>({
     endpoint = endpoint.slice(1);
   }
 
-  const url = new URL(
-    `https://strapi-production-4fd9.up.railway.app/api/${endpoint}`
-  );
+  const url = new URL(`${import.meta.env.STRAPI_URL}/api/${endpoint}`);
 
   if (query) {
     Object.entries(query).forEach(([key, value]) => {
